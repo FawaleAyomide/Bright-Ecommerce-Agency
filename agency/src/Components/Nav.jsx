580840a552029136534245de336092645c8454f9
Ayomide Oladele <fawaleayomideoladele@gmail.com>
2024-04-26 15:20:57 +0100

    update

--- agency/src/Components/Nav.jsx
+++ agency/src/Components/Nav.jsx
@@ -1,26 +1,34 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { useState } from 'react';
 import { NavLink, Link } from "react-router-dom";
+import { HiMenuAlt3 } from 'react-icons/hi';
+import { MdClose } from 'react-icons/md';
 
 const Nav = () => {
     const [showNavbar, setShowNavbar] =  useState(false);
 
+    const menuShow = () => {
+        setShowNavbar(!showNavbar);
+    }
+
   return (
     <>
     <div className='header'>
-      <nav
-        className="navbar navbar-expand 
-        navbar-light bg-light"
-      >
+    <HiMenuAlt3 className="ham-menu" onClick={menuShow} />
+      <nav className={showNavbar ? 'slider active' : 'slider'}>
         <Link to={"/"} class="logo">Bright Ecommerce Agency</Link>
-
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="/">Home
-            <span class="sr-only">(current)</span>
-            </a>
-          </li>
+        <ul className="nav-links">
+          <div className="close-icon">
+            <MdClose className="ham-close" onClick={menuShow} />
+          </div>
+          <li><NavLink className='a' to='/service'>Service</NavLink></li>
+          <li><NavLink className='a' to='/pricing'>Pricing</NavLink></li>
+          <li><NavLink className='a' to='/about'>About us</NavLink></li>
+          <li><NavLink className='a' to='/contact'>Contact us</NavLink></li>
         </ul>
+        <div>
+        <NavLink to='/getStarted'><button className="btn btn-1">Get Started</button></NavLink>
+        </div>
       </nav>
     </div>
     </>
--- agency/src/Router/Pages.jsx
+++ agency/src/Router/Pages.jsx
@@ -1,10 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../Components/HomePage';
+import Pricing from '../Components/Pricing';
+import Service from '../Components/Service';
+import About from '../Components/About';
+import Contact from '../Components/Contact';
 
 const Pages = () => {
   return (
     <Routes>
         <Route path='/' element={<HomePage/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/service' element={<Service/>} />
+        <Route path='/pricing' element={<Pricing/>} />
+        <Route path='/contact' element={<Contact/>} />
     </Routes>
   )
 }
